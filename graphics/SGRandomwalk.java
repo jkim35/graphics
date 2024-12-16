cfce988d5f9d0e62eab90b5ad40362625fd85d06
jkim35 <155586579+jkim35@users.noreply.github.com>
2024-12-16 14:30:23 -0500

    eerwq
    qwer

--- graphics/SGRandomwalk.java
+++ graphics/SGRandomwalk.java
@@ -11,16 +11,17 @@ public class SGRandomwalk {
         int xIncrease = (int)((Math.random())*20)-10;
         int yIncrease = (int)((Math.random())*20)-10;
         player.translate(xIncrease,yIncrease);
-        double distance = Math.sqrt(Math.pow(player.getX()-155,2)+Math.pow(player.getY()-155,2));
+        double distance = Math.sqrt(Math.pow(player.getX()-150,2)+Math.pow(player.getY()-150,2));
         System.out.println(distance);
-        while(distance<=100){
+        while(distance<=110){
              xIncrease = (int)((Math.random())*20)-10;
          yIncrease = (int)((Math.random())*20)-10;
         player.translate(xIncrease,yIncrease);
-         distance = Math.sqrt(Math.pow(player.getX()-150,2)+Math.pow(player.getY()-150,2));
-        System.out.println(distance);
         try { Thread.sleep(100);}
         catch(Exception ex) {};
+         distance = Math.sqrt(Math.pow(player.getX()-150,2)+Math.pow(player.getY()-150,2));
+        System.out.println(distance);
+        
 
         }
     }
